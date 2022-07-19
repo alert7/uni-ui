1721f5a992f07e877315036c99dc961ba042fde5
zhangchunming <1907723790@qq.com>
2022-07-19 17:39:53 +0800

    init

--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -103,32 +103,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "recyclableRender", function() { return recyclableRender; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "components", function() { return components; });
 var components
-try {
-  components = {
-    uniList: function() {
-      return __webpack_require__.e(/*! import() | uni_modules/uni-list/components/uni-list/uni-list */ "uni_modules/uni-list/components/uni-list/uni-list").then(__webpack_require__.bind(null, /*! @/uni_modules/uni-list/components/uni-list/uni-list.vue */ 54))
-    },
-    uniListChat: function() {
-      return __webpack_require__.e(/*! import() | uni_modules/uni-list/components/uni-list-chat/uni-list-chat */ "uni_modules/uni-list/components/uni-list-chat/uni-list-chat").then(__webpack_require__.bind(null, /*! @/uni_modules/uni-list/components/uni-list-chat/uni-list-chat.vue */ 61))
-    }
-  }
-} catch (e) {
-  if (
-    e.message.indexOf("Cannot find module") !== -1 &&
-    e.message.indexOf(".vue") !== -1
-  ) {
-    console.error(e.message)
-    console.error("1. 排查组件名称拼写是否正确")
-    console.error(
-      "2. 排查组件是否符合 easycom 规范，文档：https://uniapp.dcloud.net.cn/collocation/pages?id=easycom"
-    )
-    console.error(
-      "3. 若组件不符合 easycom 规范，需手动引入，并在 components 中注册该组件"
-    )
-  } else {
-    throw e
-  }
-}
 var render = function() {
   var _vm = this
   var _h = _vm.$createElement
@@ -164,25 +138,7 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;
 
 
 
@@ -191,158 +147,16 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var _request = _interopRequireDefault(__webpack_require__(/*! utils/request */ 18));
-var _amapWx = _interopRequireDefault(__webpack_require__(/*! utils/amap-wx.130.js */ 19));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _toConsumableArray(arr) {return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();}function _nonIterableSpread() {throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}function _unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return _arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);}function _iterableToArray(iter) {if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter);}function _arrayWithoutHoles(arr) {if (Array.isArray(arr)) return _arrayLikeToArray(arr);}function _arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}var _default =
-{
-  data: function data() {
-    return {
-      searchValue: '',
-      isFocus: false,
-      triggered: false,
-      foodSearchList: [{
-        title: '如何做一道好吃的菜肴11',
-        avatar: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png',
-        note: '高端的食材往往采用最朴素的烹饪方式',
-        time: '2020-02-02 20:20' }],
-
-
-      markers: [],
-      latitude: '',
-      longitude: '',
-      textData: {} };
-
+var _amapWx = _interopRequireDefault(__webpack_require__(/*! utils/amap-wx.130.js */ 19));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} //
+//
+//
+//
+//
+//
+//
+var _default = { data: function data() {return {};}, onLoad: function onLoad() {
   },
-  onLoad: function onLoad() {
-    console.log('amapFile---------', _amapWx.default);
-    this.getLocation();
-    this.login();
-    this._freshing = false;
-    for (var i = 0; i < 10; i++) {
-      this.foodSearchList = [].concat(_toConsumableArray(this.foodSearchList), [{
-        title: '如何做一道好吃的菜肴11',
-        avatar: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png',
-        note: '高端的食材往往采用最朴素的烹饪方式',
-        time: '2020-02-02 20:20' }]);
-
-    }
-    // setTimeout(() => {
-    // 	this.triggered = true;
-    // }, 1000)
-  },
-  methods: {
-    getLocation: function getLocation() {
-      var that = this;
-      var myAmapFun = new _amapWx.default.AMapWX({
-        key: 'fe303bc170209c7c18b082da2f35acd4' });
-
-      myAmapFun.getRegeo({
-        iconPath: "../../img/marker.png",
-        iconWidth: 22,
-        iconHeight: 32,
-        success: function success(data) {
-          var marker = [{
-            id: data[0].id,
-            latitude: data[0].latitude,
-            longitude: data[0].longitude,
-            iconPath: data[0].iconPath,
-            width: data[0].width,
-            height: data[0].height }];
-
-          that.markers = marker;
-          that.latitude = data[0].latitude;
-          that.longitude = data[0].longitude;
-          that.textData = {
-            name: data[0].name,
-            desc: data[0].desc };
-
-        },
-        fail: function fail(info) {
-          // wx.showModal({title:info.errMsg})
-        } });
-
-    },
-    login: function login() {
-      var _self = this;
-      uni.showLoading({
-        title: "加载中..." });
-
-      uni.login({
-        provider: "weixin",
-        onlyAuthorize: true,
-        success: function success(loginRes) {
-          console.log('loginRes', loginRes);
-          var data = {
-            code: loginRes.code };
-
-          (0, _request.default)("/wx/getAccessToken", data).then(function (res) {
-            console.log(res);
-            uni.setStorageSync("openid", res.openid);
-            uni.setStorageSync("session_key", res.session_key);
-            uni.hideLoading();
-          });
-        } });
-
-    },
-    getUserProfile: function getUserProfile(e) {
-      var _self = this;
-      uni.getUserProfile({
-        desc: "用于完善会员资料", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: function success(res) {
-          var userInfo = res.userInfo;
-          var data = {
-            encryptedData: res.encryptedData,
-            iv: res.iv,
-            sessionKey: uni.getStorageSync("session_key"),
-            openId: uni.getStorageSync("openid") };
-
-          _self.$request("/wx/userinfo", data).then(function (ret) {
-            uni.setStorageSync("avatar", ret.avatarUrl);
-            uni.setStorageSync("nickname", ret.nickName);
-            setTimeout(function () {
-              uni.switchTab({
-                url: "/pages/mine/index" });
-
-            }, 50);
-          });
-        } });
-
-    },
-
-    search: function search(res) {
-      uni.showToast({
-        title: '搜索：' + res.value,
-        icon: 'none' });
-
-    },
-    input: function input(res) {
-      console.log('----input:', res);
-    },
-    clear: function clear(res) {
-      uni.showToast({
-        title: 'clear事件，清除值为：' + res.value,
-        icon: 'none' });
-
-    },
-    blur: function blur(res) {
-
-    },
-    focus: function focus(e) {
-      this.isFocus = true;
-    },
-    cancel: function cancel(res) {
-      this.isFocus = false;
-    },
-    onRefresh: function onRefresh() {var _this = this;
-      if (this._freshing) return;
-      uni.showToast({
-        title: '加载成功' });
-
-      this._freshing = true;
-      setTimeout(function () {
-        _this.triggered = false;
-        _this._freshing = false;
-      }, 3000);
-    } } };exports.default = _default;
-/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
+  methods: {} };exports.default = _default;
 
 /***/ }),
 /* 18 */,
